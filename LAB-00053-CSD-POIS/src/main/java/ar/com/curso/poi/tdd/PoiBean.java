43ed090c02f096b15c61f47f7f29494a058d2b87
Unknown <torresgabo@gmail.com>
2017-08-01 11:30:39 -0500

    PoiBean que retorna ArrayList de pois estaticos

--- LAB-00053-CSD-POIS/src/main/java/ar/com/curso/poi/tdd/PoiBean.java
+++ LAB-00053-CSD-POIS/src/main/java/ar/com/curso/poi/tdd/PoiBean.java
@@ -1,5 +1,8 @@
 package ar.com.curso.poi.tdd;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author Administrador
  *
@@ -9,39 +12,89 @@ public class PoiBean {
 	private String latitud;
 	private String nombre;
 	private String descripcion;
-	
-//	-27.7815141,-64.2645084 Pizzería "Las Cuartetas" Pizzería "Las Cuartetas" 
-//	-34.5978366,-58.3876505 El Cuartito 
-//	-34.6033254,-58.3795288 El Palacio de la Pizza 
-//	-34.6088526,-58.3759733 Cabildo de Buenos Aires 
-	
+
 	public PoiBean() {
 
 	}
+
 	public String getLongitud() {
 		return longitud;
 	}
+
 	public void setLongitud(String longitud) {
 		this.longitud = longitud;
 	}
+
 	public String getLatitud() {
 		return latitud;
 	}
+
 	public void setLatitud(String latitud) {
 		this.latitud = latitud;
 	}
+
 	public String getDescripcion() {
 		return descripcion;
 	}
+
 	public void setDescripcion(String descripcion) {
 		this.descripcion = descripcion;
 	}
+
 	public String getNombre() {
 		return nombre;
 	}
+
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
-	
-	
+
+	public List<PoiBean> getPois() {
+
+		List<PoiBean> pois = new ArrayList();
+
+		PoiBean item = new PoiBean();
+		// long        latitud
+		// -27.7815141,-64.2645084 Pizzería "Las Cuartetas"
+		// -34.5978366,-58.3876505 El Cuartito
+		// -34.6033254,-58.3795288 El Palacio de la Pizza
+		// -34.6088526,-58.3759733 Cabildo de Buenos Aires
+
+		item.descripcion = "Pizzería 'Las Cuartetas'";
+		item.latitud = "-64.2645084";
+		item.longitud = "-27.7815141";
+		item.nombre = "Pizzería 'Las Cuartetas'";
+
+		pois.add(item);
+
+		item = new PoiBean();
+
+		item.descripcion = "El Cuartito";
+		item.latitud = "-58.3876505";
+		item.longitud = "-34.5978366";
+		item.nombre = "El Cuartito";
+
+		pois.add(item);
+
+		item = new PoiBean();
+
+		item.descripcion = "El Palacio de la Pizza";
+		item.latitud = "-58.3795288";
+		item.longitud = "-34.6033254";
+		item.nombre = "El Palacio de la Pizza";
+
+		pois.add(item);
+
+		item = new PoiBean();
+
+		item.descripcion = "Cabildo de Buenos Aires";
+		item.latitud = "-34.6088526";
+		item.longitud = "-58.3759733";
+		item.nombre = "Cabildo de Buenos Aires";
+
+		pois.add(item);
+
+		return pois;
+	}
+
 }

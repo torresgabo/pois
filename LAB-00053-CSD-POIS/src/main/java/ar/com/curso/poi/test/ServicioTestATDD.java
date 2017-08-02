0a2918d7381e811b0fc9258ed09f1e524843361f
Javier Miranda <1990jmirandar@gmail.com>
2017-08-02 11:19:19 -0500

    Auto stash before merge of "master" and "origin/master"
    commit

--- LAB-00053-CSD-POIS/src/main/java/ar/com/curso/poi/test/ServicioTestATDD.java
+++ LAB-00053-CSD-POIS/src/main/java/ar/com/curso/poi/test/ServicioTestATDD.java
@@ -2,6 +2,11 @@ package ar.com.curso.poi.test;
 
 import static org.assertj.core.api.Assertions.*;
 
+import java.io.DataInputStream;
+import java.io.PrintStream;
+import java.net.URL;
+import java.net.URLConnection;
+
 import org.junit.Test;
 
 import ar.com.curso.poi.controladores.ControladorHome;
@@ -11,9 +16,26 @@ public class ServicioTestATDD {
 	ControladorHome controlador = new ControladorHome();
 	@Test
 	public void validaElPoiMasCercano(){
-		String url = "htto://localhost:8080/pois-api/Buenos_Aires_Turismo/latitud/longitud";
+		//Obelisco
+		//lon -34.6037389
+		//lat -58.3837644
+		String url = "htto://localhost:8080/pois-api/Buenos_Aires_Turismo/-34.6037389/-58.3837644";
+		try {
+			String data=llamarUrl(url);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		assertThat(controlador.validaServicio("Argentina")).isFalse();
 	}
+	
+	public String llamarUrl(String url) throws Exception{
+
+        URL urlConnect = new URL(url);
+        URLConnection connection = urlConnect.openConnection();
+        DataInputStream inStream = new DataInputStream(connection.getInputStream());
+        return inStream.readLine();
+	} 
 	
 	
 }

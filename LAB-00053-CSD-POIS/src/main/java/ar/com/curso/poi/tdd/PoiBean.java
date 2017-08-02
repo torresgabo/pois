569769b62d30fda9dbe40a210052f84495bd683a
Unknown <torresgabo@gmail.com>
2017-08-02 11:11:52 -0500

    Auto stash before rebase of "origin/master"
    PoiBean Actualizado

--- LAB-00053-CSD-POIS/src/main/java/ar/com/curso/poi/tdd/PoiBean.java
+++ LAB-00053-CSD-POIS/src/main/java/ar/com/curso/poi/tdd/PoiBean.java
@@ -75,15 +75,16 @@ public class PoiBean {
 		List<PoiBean> pois = new ArrayList();
 
 		PoiBean item = new PoiBean();
-		// long latitud
-		// -27.7815141,-64.2645084 Pizzería "Las Cuartetas"
-		// -34.5978366,-58.3876505 El Cuartito
-		// -34.6033254,-58.3795288 El Palacio de la Pizza
-		// -34.6088526,-58.3759733 Cabildo de Buenos Aires
+		// 		long 		latitud
+		//		-34.6033254,-58.3795235 Pizzería "Las Cuartetas"
+		//		-34.5980574,-58.3870766 El Cuartito 
+		//		-34.6041472,-58.3771743 El Palacio de la Pizza 
+		//		-34.6088526,-58.3759679 Cabildo de Buenos Aires
+
 
 		item.descripcion = "Pizzería 'Las Cuartetas'";
-		item.latitud = "-64.2645084";
-		item.longitud = "-27.7815141";
+		item.latitud = "-58.3795235";
+		item.longitud = "-34.6033254";
 		item.nombre = "Pizzería 'Las Cuartetas'";
 		item.servicio = "Buenos_Aires_Turismo";
 
@@ -92,8 +93,8 @@ public class PoiBean {
 		item = new PoiBean();
 
 		item.descripcion = "El Cuartito";
-		item.latitud = "-58.3876505";
-		item.longitud = "-34.5978366";
+		item.latitud = "-58.3870766";
+		item.longitud = "-34.5980574";
 		item.nombre = "El Cuartito";
 		item.servicio = "Buenos_Aires_Turismo";
 
@@ -102,8 +103,8 @@ public class PoiBean {
 		item = new PoiBean();
 
 		item.descripcion = "El Palacio de la Pizza";
-		item.latitud = "-58.3795288";
-		item.longitud = "-34.6033254";
+		item.latitud = "-58.3771743";
+		item.longitud = "-34.6041472";
 		item.nombre = "El Palacio de la Pizza";
 		item.servicio = "Buenos_Aires_Turismo";
 
@@ -112,7 +113,7 @@ public class PoiBean {
 		item = new PoiBean();
 
 		item.descripcion = "Cabildo de Buenos Aires";
-		item.latitud = "-58.3759733";
+		item.latitud = "-58.3759679";
 		item.longitud = "-34.6088526";
 		item.nombre = "Cabildo de Buenos Aires";
 		item.servicio = "Buenos_Aires_Turismo";
